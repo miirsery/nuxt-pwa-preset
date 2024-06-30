a7aa8068b7ceab6bc7f3796b66978a206c5f961a
miirsery <miirsery.main@gmail.com>
2024-06-30 20:38:23 +0700

    fix nxut config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
+
+const sw = process.env.SW === 'true'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@vite-pwa/nuxt"],
   pwa: {
-    mode: "development",
+    strategies: sw ? 'injectManifest' : 'generateSW',
+    srcDir: sw ? 'service-worker' : undefined,
+    filename: sw ? 'sw.ts' : undefined,
+    registerType: 'autoUpdate',
     manifest: {
       name: "Nuxt App",
       short_name: "Nuxt App",
@@ -35,11 +41,23 @@ export default defineNuxtConfig({
       ]
     },
     workbox: {
-      navigateFallback: "/",
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
     },
     devOptions: {
       enabled: true,
-      type: "module"
-    }
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
   },
 })
