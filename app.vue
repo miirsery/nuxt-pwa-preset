e00fd0c742fbcf87a9ebe631263f671b70387ec2
Nermitor <77717652+Nermitor@users.noreply.github.com>
2024-06-30 21:15:16 +0700

    Update (#1)

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtPwaManifest />
+    <NuxtPwaAssets />
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
 
-const sw = process.env.SW === 'true'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@vite-pwa/nuxt"],
   pwa: {
-    strategies: sw ? 'injectManifest' : 'generateSW',
-    srcDir: sw ? 'service-worker' : undefined,
-    filename: sw ? 'sw.ts' : undefined,
     registerType: 'autoUpdate',
     manifest: {
       name: "Nuxt App",
@@ -16,7 +12,7 @@ export default defineNuxtConfig({
       description: "Nuxt App",
       theme_color: "#ffffff",
       orientation: 'natural',
-      display: 'standalone',
+      display: 'fullscreen',
       icons: [{
         "src": "qwe-192.png",
         "sizes": "192x192",
@@ -46,16 +42,8 @@ export default defineNuxtConfig({
     injectManifest: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
     },
-    client: {
-      installPrompt: true,
-      // you don't need to include this: only for testing purposes
-      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-      periodicSyncForUpdates: 20,
-    },
     devOptions: {
       enabled: true,
-      suppressWarnings: true,
-      navigateFallback: '/',
       navigateFallbackAllowlist: [/^\/$/],
       type: 'module',
     },
